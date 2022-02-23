43792260b963447899d87624297c0accec75509c
Łukasz Żytko <zytko.lukasz@gmail.com>
2022-02-23 14:44:55 +0100

    Add fake posts generator

---
+++ fake-posts.py
@@ -0,0 +1,17 @@
+from faker import Faker
+from blog.models import Entry, db
+
+def fake_entries(how_many=10):
+    fake = Faker()
+
+    for i in range(how_many):
+        post = Entry(
+            title = fake.sentence(),
+            body = "\n".join(fake.paragraphs(15)),
+            pub_date = fake.date_between(start_date="-3y"),
+            is_published = True 
+        )
+        db.session.add(post)
+    db.session.commit()
+
+fake_entries(how_many=5)
