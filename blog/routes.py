740427807987bbd831a2bd03dde4931523553b90
Łukasz Żytko <zytko.lukasz@gmail.com>
2022-02-24 14:19:30 +0100

    Refactor post modification view

--- blog/routes.py
+++ blog/routes.py
@@ -8,40 +8,37 @@ def home():
     all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
     return render_template("homepage.html", all_posts = all_posts)
 
-
 @app.route("/add-post/", methods=["GET", "POST"])
-def create_entry():
-    form = EntryForm()
-    errors = None
-    typ = "Dodaj nowy"
-    if request.method == "POST":
-        if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data, 
-                body=form.body.data,
-                is_published = form.is_published.data
-            )
-            db.session.add(entry)
-            db.session.commit()
-            if form.is_published.data == True:
-                flash(f'Wpis "{form.title.data}" został opublikowany')
-            else:
-                flash(f'Wpis "{form.title.data}" został dodany bez publikacji')
-        else:
-            errors = form.errors
-    return render_template("post.html", form=form, errors=errors, type=typ)
-
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id):
-    typ = "Modyfikuj"
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    form = EntryForm(obj=entry)
+def add_mod_entry(entry_id=None):
     errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-            flash(f'Wpis "{form.title.data}" został zmieniony')
-        else:
-            errors = form.errors
-    return render_template("post.html", form=form, errors=errors, type=typ)
+    if not entry_id:
+        action = "Dodaj nowy"
+        form = form = EntryForm()
+    else:
+        action = "Modyfikuj"
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+    if request.method == "POST":
+        if not entry_id:
+            if form.validate_on_submit():
+                entry = Entry(
+                    title=form.title.data, 
+                    body=form.body.data,
+                    is_published = form.is_published.data
+                )
+                db.session.add(entry)
+                if form.is_published.data == True:
+                    flash(f'Wpis "{form.title.data}" został opublikowany')
+                else:
+                    flash(f'Wpis "{form.title.data}" został dodany bez publikacji')
+            else:
+                erros = form.errors
+        elif entry_id:
+            if form.validate_on_submit():
+                form.populate_obj(entry)
+                flash(f'Wpis "{form.title.data}" został zmodyfikowany')
+            else:
+                errors = form.errors
+        db.session.commit()
+    return render_template("post.html", form=form, errors=errors, action=action)    
